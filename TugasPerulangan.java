fbf2814db43f1bb1e22cbbfafad181304c801a03
Sammlana <jokolkkpk@gmail.com>
2024-04-30 08:14:08 +0800

    1

---
+++ TugasPerulangan.java
@@ -0,0 +1,64 @@
+import java.util.Scanner;
+
+public class TugasPerulangan {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Masukkan nama mahasiswa: ");
+        String nama = scanner.nextLine();
+
+        System.out.print("Masukkan nilai Kehadiran: ");
+        double nilket = scanner.nextDouble();
+        
+        System.out.print("Masukkan nilai Tugas: ");
+        double niltug = scanner.nextDouble();
+
+        System.out.print("Masukkan nilai UTS: ");
+        double niluts = scanner.nextDouble();
+
+        System.out.print("Masukkan nilai UAS: ");
+        double niluas = scanner.nextDouble();
+
+        double nilai = 0;
+
+
+
+        char grade;
+
+        if (nilai >= 0 && nilai <= 100) {
+            if (nilai >= 85) {
+                grade = 'A';
+            } else if (nilai >= 70) {
+                grade = 'B';
+            } else if (nilai >= 55) {
+                grade = 'C';
+            } else if (nilai >= 40) {
+                grade = 'D';
+            } else {
+                grade = 'E';
+            }
+
+            switch (grade) {
+                case 'A':
+                    System.out.println("Selamat, " + nama + " mendapat nilai A!");
+                    break;
+                case 'B':
+                    System.out.println("Bagus, " + nama + " mendapat nilai B.");
+                    break;
+                case 'C':
+                    System.out.println("Cukup, " + nama + " mendapat nilai C.");
+                    break;
+                case 'D':
+                    System.out.println("Anda perlu memperbaiki, " + nama + " mendapat nilai D.");
+                    break;
+                case 'E':
+                    System.out.println("Maaf, " + nama + " mendapat nilai E.");
+                    break;
+                default:
+                    System.out.println("Grade tidak valid.");
+            }
+        } else {
+            System.out.println("Nilai tidak valid. Harap masukkan nilai antara 0 dan 100.");
+        }
+    }
+}
